a1270d4e32c187e7ac8b80ddaef894f79dc0cd9a
David Golden <dagolden@cpan.org>
2015-01-12 21:24:15 -0500

    add synopsis in examples folder

---
+++ examples/synopsis.pl
@@ -0,0 +1,20 @@
+use v5.10;
+use strict;
+use warnings;
+
+use Data::Fake qw/Core Names Text Dates/;
+use Data::Dumper;
+
+my $hero_factory = fake_hash(
+    {
+        name      => fake_name(),
+        battlecry => fake_sentences(1),
+        birthday  => fake_past_datetime("%Y-%m-%d"),
+        friends   => fake_var_array( 3, 6, fake_name() ),
+        gender    => fake_choice(qw/Male Female Other/),
+    }
+);
+
+my $hero = $hero_factory->();
+
+say Dumper($hero);
