88a932638440a6c45dcb4392c19e956986478937
David Golden <dagolden@cpan.org>
2015-01-21 14:15:25 -0500

    fix SYNOPSIS and example file

--- examples/synopsis.pl
+++ examples/synopsis.pl
@@ -10,7 +10,7 @@ my $hero_factory = fake_hash(
         name      => fake_name(),
         battlecry => fake_sentences(1),
         birthday  => fake_past_datetime("%Y-%m-%d"),
-        friends   => fake_var_array( 3, 6, fake_name() ),
+        friends   => fake_array( fake_int( 2, 4 ), fake_name() ),
         gender    => fake_pick(qw/Male Female Other/),
     }
 );
--- lib/Data/Fake.pm
+++ lib/Data/Fake.pm
@@ -30,7 +30,7 @@ sub import {
             name      => fake_name(),
             battlecry => fake_sentences(1),
             birthday  => fake_past_date("%Y-%m-%d"),
-            friends   => fake_var_array( 3, 6, fake_name() ),
+            friends   => fake_array( fake_int(2,4), fake_name() ),
             gender    => fake_pick(qw/Male Female Other/),
         }
     );
